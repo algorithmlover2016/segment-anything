fa68bb09fcb6e90a726181aae324724a156cb9e5
alogirhtmlover <yubo.upt@gmail.com>
2023-04-20 10:24:09 +0800

    config the para

--- scripts/sam_test.py
+++ scripts/sam_test.py
@@ -1,22 +1,26 @@
-from segment_anything import build_sam, SamAutomaticMaskGenerator, build_sam_vit_l, build_sam_vit_b
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 import pytest
 import cv2
 
 model_weights = {
-        "H" : "../weights/sam_vit_h_4b8939.pth",
-        "B" : "../weights/sam_vit_b_01ec64.pth",
-        "L" : "../weights/sam_vit_l_0b3195.pth",
+        "vit_h" : "../weights/sam_vit_h_4b8939.pth",
+        "vit_b" : "../weights/sam_vit_b_01ec64.pth",
+        "vit_l" : "../weights/sam_vit_l_0b3195.pth",
 }
 image3 = cv2.imread("./testImgs/3.jpg")
 image3 = cv2.cvtColor(image3, cv2.COLOR_BGR2RGB)
 imagewall2 = cv2.imread("./testImgs/wall2.jpg")
 imagewall2 = cv2.cvtColor(imagewall2, cv2.COLOR_BGR2RGB)
 
-mask_generatorL = SamAutomaticMaskGenerator(build_sam_vit_l(checkpoint=model_weights['L']).to("cuda"))
+model_type = 'vit_l'
+mask_generatorL = SamAutomaticMaskGenerator(sam_model_registry[model_type](checkpoint=model_weights[model_type]).to("cuda"))
+
+model_type = 'vit_b'
 # masks = mask_generatorL.generate(image3)
-mask_generatorB = SamAutomaticMaskGenerator(build_sam_vit_b(checkpoint=model_weights['B']).to("cuda"))
+mask_generatorB = SamAutomaticMaskGenerator(sam_model_registry[model_type](checkpoint=model_weights[model_type]).to("cuda"))
 # masks = mask_generatorB.generate(image3)
-mask_generatorH = SamAutomaticMaskGenerator(build_sam(checkpoint=model_weights['H']).to("cuda"))
+model_type = 'vit_h'
+mask_generatorH = SamAutomaticMaskGenerator(sam_model_registry[model_type](checkpoint=model_weights[model_type]).to("cuda"))
 # masks = mask_generatorH.generate(image3)
 
 
@@ -24,11 +28,11 @@ def sam_modelsL():
     masks = mask_generatorL.generate(image3)
     masks = mask_generatorL.generate(imagewall2)
 
-def sam_modelsB(model_type = "B"):
+def sam_modelsB(model_type = "vit_b"):
     masks = mask_generatorB.generate(image3)
     masks = mask_generatorB.generate(imagewall2)
 
-def sam_modelsH(model_type = "H"):
+def sam_modelsH(model_type = "vit_h"):
     masks = mask_generatorH.generate(image3)
     masks = mask_generatorH.generate(imagewall2)
 
